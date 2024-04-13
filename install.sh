0941283bfc8b630e995baed5f0faca8e8313f875
frib3iro <rib3iro@live.com>
2024-04-13 13:21:58 -0300

    Arquivo install incompleto!

--- install.sh
+++ install.sh
@@ -8,40 +8,54 @@
 # Data      : 11/12/2020
 # Licença   : GNU/GPL v3.0
 # --------------------------------------------------------
-# Uso       : ./install-01.sh
+# Uso       : ./install.sh
 # --------------------------------------------------------
 
-echo "Instalando o yay..."
+# Atualizando o sistema ----------------------------------
+echo "Atualizando todo o sistema..."
 sleep 2s
+sudo pacman -Syu --noconfirm
 
+# Instalando as fontes -----------------------------------
+echo "Instalando as fontes..."
+sleep 2s
+sudo pacman -S ttf-opensans
+sudo pacman -S ttf-mononoki-nerd
+
+# Instalando pacotes -------------------------------------
+echo "Instalando pacotes..."
+sleep 2s
 sudo pacman -S git go --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ~/ && rm -rf yay
 
-echo "Instalando as fontes..."
+sudo pacman -S thunar thunar-media-tags-plugin thunar-archive-plugin thunar-volman
+
+# Copiando os arquivos para os diretórios ---------------
+echo "Copiando os arquivos para os diretórios..."
 sleep 2s
 
-sudo pacman -S ttf-opensans
-sudo pacman -S ttf-mononoki-nerd
-
-# Configurações
+# Fazendo as configurações -------------------------------
+echo "Fazendo as configuraçẽs..."
+sleep 2s
 ~/.config/i3/config
 ~/.config/kitty
 /etc/lightdm
 /etc/xdg/picon.conf
 
-# Exemplo plugins e temas no vim
-mkdir -p ~/.vim/pack/themes/start/ 
+# Instalando  plugins e temas no vim
 echo "Baixando os temas para o vim..."
 sleep 2s
-cd ~/.vim/pack/themes/start/ 
-git clone https://github.com/ghifarit53/tokyonight-vim.git
-git clone https://github.com/joshdick/onedark.vim.git
 
-cd ~/
-mkdir -p ~/.vim/pack/plugins/start/ 
+mkdir -p ~/.vim/pack/plugins/start && git clone https://github.com/mg979/vim-visual-multi.git
 
+mkdir -p /home/fabio/.vim/pack/plugins/start
+
+mkdir -p /home/fabio/.vim/pack/themes/start &&  git clone https://github.com/ghifarit53/tokyonight-vim.git &&  git clone https://github.com/joshdick/onedark.vim.git
+
+mkdir -p /home/fabio/.vim/view
 
 
 
